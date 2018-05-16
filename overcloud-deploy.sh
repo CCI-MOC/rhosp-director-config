a514fb139195f4e669d4b6e0b24d1d741487b61d
Lars Kellogg-Stedman <lars@redhat.com>
2018-05-16 13:45:40 -0400

    added overcloud deploy script

---
+++ overcloud-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+TEMPLATES=/usr/share/openstack-tripleo-heat-templates
+
+# When passing environment files (`-e ...`) to the `overcloud deploy`
+# command, order is important! Your custom configuration
+# (`$PWD/templates/deploy.yaml` in this script) should come *last*,
+# in particular because the network interface configuration is
+# initialized by `deployed-server-environment.yaml`.  If you custom
+# configuration were provided first, the network interface
+# configuration you provide would be lost and replaced with the
+# defaults.
+
+openstack overcloud deploy \
+	--templates $TEMPLATES \
+	--disable-validations --deployed-server \
+	--libvirt-type kvm \
+	--ntp-server pool.ntp.org \
+	-n $PWD/templates/network/network_data.yaml \
+	-e $TEMPLATES/environments/network-isolation.yaml \
+	-e $TEMPLATES/environments/deployed-server-environment.yaml \
+	-e $TEMPLATES/environments/deployed-server-bootstrap-environment-rhel.yaml \
+	-e $TEMPLATES/environments/deployed-server-pacemaker-environment.yaml \
+	-r $TEMPLATES/deployed-server/deployed-server-roles-data.yaml \
+	-e $PWD/templates/deploy.yaml
