4a5ee4068bd47ae7e7027926361aad4c0ac7e95e
Lars Kellogg-Stedman <lars@redhat.com>
2018-06-06 19:46:51 -0400

    document deploy arguments in overcloud-deploy.sh
    this modifies overcloud-deploy.sh so that we can add comments to
    individual command line arguments to make it more obvious why we're
    including various environment files.

--- overcloud-deploy.sh
+++ overcloud-deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 TEMPLATES=/usr/share/openstack-tripleo-heat-templates
 
@@ -6,7 +6,7 @@ TEMPLATES=/usr/share/openstack-tripleo-heat-templates
 # command, order is important! Your custom configuration
 # (`$PWD/templates/deploy.yaml` in this script) should come *last*,
 # in particular because the network interface configuration is
-# initialized by `deployed-server-environment.yaml`.  If you custom
+# initialized by `deployed-server-environment.yaml`.  If your custom
 # configuration were provided first, the network interface
 # configuration you provide would be lost and replaced with the
 # defaults.
@@ -14,20 +14,43 @@ TEMPLATES=/usr/share/openstack-tripleo-heat-templates
 # clean up any old credentials
 rm -f overcloudrc overcloudrc.v3
 
+deploy_args=(
+	# network_data.yaml defines a custom network configuration. In 
+	# particular, we disable the StorageMgmt network, since we're not
+	# using it.
+	-n $PWD/templates/network/network_data.yaml
+
+	# Enable network isolation (using different networks for different
+	# purposes).
+	-e $TEMPLATES/environments/network-isolation.yaml
+
+	# Enable deployment onto pre-provisioned servers.
+	-e $TEMPLATES/environments/deployed-server-environment.yaml
+	-e $TEMPLATES/environments/deployed-server-bootstrap-environment-rhel.yaml
+	-e $TEMPLATES/environments/deployed-server-pacemaker-environment.yaml
+	-r $TEMPLATES/deployed-server/deployed-server-roles-data.yaml
+
+	# Enable TLS for public endpoints.
+	-e $TEMPLATES/environments/ssl/enable-tls.yaml
+	-e $TEMPLATES/environments/tls-endpoints-public-dns.yaml
+
+	# Use Docker registry on the undercloud.
+	-e $PWD/templates/overcloud_images.yaml
+
+	# Most of our custom configuration.
+	-e $PWD/templates/deploy.yaml
+
+	# Support for an external Ceph cluster.
+	-e $PWD/templates/ceph-external.yaml
+
+	# Passwords and other credentials (this file is not included in
+	# the repository).
+	-e $PWD/templates/credentials.yaml
+)
+
 openstack overcloud deploy \
 	--templates $TEMPLATES \
 	--disable-validations --deployed-server \
 	--libvirt-type kvm \
 	--ntp-server pool.ntp.org \
-	-n $PWD/templates/network/network_data.yaml \
-	-e $TEMPLATES/environments/network-isolation.yaml \
-	-e $TEMPLATES/environments/deployed-server-environment.yaml \
-	-e $TEMPLATES/environments/deployed-server-bootstrap-environment-rhel.yaml \
-	-e $TEMPLATES/environments/deployed-server-pacemaker-environment.yaml \
-	-e $TEMPLATES/environments/ssl/enable-tls.yaml \
-	-e $TEMPLATES/environments/tls-endpoints-public-dns.yaml \
-	-r $TEMPLATES/deployed-server/deployed-server-roles-data.yaml \
-	-e $PWD/templates/overcloud_images.yaml \
-	-e $PWD/templates/deploy.yaml \
-	-e $PWD/templates/ceph-external.yaml \
-	-e $PWD/templates/credentials.yaml
+	"${deploy_args[@]}"
