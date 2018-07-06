a3c823f4bd7171c516477e9a160c947521dc841e
Lars Kellogg-Stedman <lars@redhat.com>
2018-07-05 21:38:52 -0400

    update overcloud-prep to use rhosp13 images

--- overcloud-prep.sh
+++ overcloud-prep.sh
@@ -1,26 +1,16 @@
 #!/bin/sh
 
-tag=$(openstack overcloud container image tag discover \
-  --image registry.access.redhat.com/rhosp12/openstack-base:latest \
-  --tag-from-label version-release)
-
-echo "using tag: $tag"
-
 openstack overcloud container image prepare \
-  --namespace=registry.access.redhat.com/rhosp12 \
-  --prefix=openstack- \
-  --tag=$tag \
+  --namespace registry.access.redhat.com/rhosp13 \
+  --push-destination 192.168.24.1:8787 \
+  --prefix openstack- \
+  --tag-from-label {version}-{release} \
+  --output-env-file $PWD/patches/docker/overcloud_images.yaml \
   --output-images-file $PWD/local_registry_images.yaml
 
 openstack overcloud container image upload \
   --config-file  $PWD/local_registry_images.yaml \
   --verbose
-
-openstack overcloud container image prepare \
-  --namespace=192.168.24.1:8787/rhosp12 \
-  --prefix=openstack- \
-  --tag=$tag \
-  --output-env-file=$PWD/patches/docker/overcloud_images.yaml
 
 # Patch the stock images with our local changes
 ./scripts/build-images-wrapper -o $PWD/templates/overcloud_images.yaml --push
