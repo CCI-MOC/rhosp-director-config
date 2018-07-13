5f9e43d6c4bd15044017083ec2928a69940fd798
Lars Kellogg-Stedman <lars@redhat.com>
2018-07-13 11:40:37 -0400

    teach overcloud-continue.sh about networker role

--- overcloud-continue.sh
+++ overcloud-continue.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export OVERCLOUD_ROLES="ControllerDeployedServer ComputeDeployedServer"
+export OVERCLOUD_ROLES="ControllerDeployedServer ComputeDeployedServer NetworkerDeployedServer"
 export ControllerDeployedServer_hosts="kumo-control-01 kumo-control-02 kumo-control-03"
 export ComputeDeployedServer_hosts="kumo-compute-01 kumo-compute-02"
+export NetworkerDeployedServer_hosts="kumo-networker-01 kumo-networker-02"
 
 /usr/share/openstack-tripleo-heat-templates/deployed-server/scripts/get-occ-config.sh
