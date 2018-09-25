10a04ddd1a667b9fff613c6b68475ad64821dc99
Lars Kellogg-Stedman <lars@redhat.com>
2018-09-25 15:36:15 -0400

    new hostnames in overcloud-continue.sh

--- overcloud-continue.sh
+++ overcloud-continue.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
 export OVERCLOUD_ROLES="ControllerDeployedServer ComputeDeployedServer NetworkerDeployedServer"
-export ControllerDeployedServer_hosts="kumo-control-01 kumo-control-02 kumo-control-03"
-export ComputeDeployedServer_hosts="kumo-compute-01 kumo-compute-02"
-export NetworkerDeployedServer_hosts="kumo-networker-01 kumo-networker-02"
+export ControllerDeployedServer_hosts="
+neu-3-39-control3.kzn.moc
+neu-5-39-control2.kzn.moc
+neu-15-39-control1.kzn.moc
+"
+export ComputeDeployedServer_hosts="
+neu-3-34-stackcomp.kzn.moc
+neu-3-35-stackcomp.kzn.moc
+neu-3-36-stackcomp.kzn.moc
+neu-3-37-stackcomp.kzn.moc
+neu-3-38-stackcomp.kzn.moc
+neu-5-34-stackcomp.kzn.moc
+neu-5-35-stackcomp.kzn.moc
+neu-5-36-stackcomp.kzn.moc
+neu-5-37-stackcomp.kzn.moc
+neu-5-38-stackcomp.kzn.moc
+"
+export NetworkerDeployedServer_hosts="
+neu-19-11-nc1.kzn.moc
+neu-17-11-nc2.kzn.moc
+"
 
 /usr/share/openstack-tripleo-heat-templates/deployed-server/scripts/get-occ-config.sh
