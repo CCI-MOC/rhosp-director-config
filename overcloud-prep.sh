615036f1830e1c194cf029c5a211fbb5eb3ef13b
hgibson1 <hgibson1@bu.edu>
2018-11-20 16:10:37 -0500

    Made changes to config

--- overcloud-prep.sh
+++ overcloud-prep.sh
@@ -2,15 +2,17 @@
 
 openstack overcloud container image prepare \
   --namespace registry.access.redhat.com/rhosp13 \
-  --push-destination 172.16.0.5:8787 \
+  --push-destination 10.255.5.4:8787 \
   --prefix openstack- \
   --tag-from-label {version}-{release} \
   --output-env-file $PWD/patches/docker/overcloud_images.yaml \
-  --output-images-file $PWD/local_registry_images.yaml
+  --output-images-file $PWD/local_registry_images.yaml \
+  --debug
 
 openstack overcloud container image upload \
   --config-file  $PWD/local_registry_images.yaml \
-  --verbose
+  --verbose \
+  --debug
 
 # Patch the stock images with our local changes
 ./scripts/build-images-wrapper -o $PWD/templates/overcloud_images.yaml --push
--- undercloud-install.sh
+++ undercloud-install.sh
@@ -2,7 +2,7 @@
 
 openstack undercloud install
 
-if sudo ovs-vsctl list-ports br-ctlplane | grep eth1; then
-	sudo ovs-vsctl del-port br-ctlplane eth1
-	sudo ovs-vsctl add-port br-ctlplane vlan3703
-fi
+#if sudo ovs-vsctl list-ports br-ctlplane | grep ibft0; then
+#	sudo ovs-vsctl del-port br-ctlplane ibft0
+#	sudo ovs-vsctl add-port br-ctlplane vlan258
+#fi
