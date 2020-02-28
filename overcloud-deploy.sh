c3658a82c1040e3da42b1bbc4ffe69ff97ff21fe
Lars Kellogg-Stedman <lars@redhat.com>
2020-02-28 08:55:41 -0500

    ensure templates are up-to-date when deploying

--- overcloud-deploy.sh
+++ overcloud-deploy.sh
@@ -13,6 +13,9 @@ else
 	TEMPLATES=/usr/share/openstack-tripleo-heat-templates
 fi
 
+# Ensure that templates are up-to-date
+(cd playbooks; ansible-playbook deploy.yml)
+
 # This is necessary to work around bugs #1645503 and #1645134
 . ./overcloud-env.sh
 bash $TEMPLATES/deployed-server/scripts/enable-ssh-admin.sh
