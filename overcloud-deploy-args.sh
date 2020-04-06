f2136909dc7db1bc974bce6c9b17cb156e9ccee9
Lars Kellogg-Stedman <lars@redhat.com>
2020-04-06 12:49:38 -0400

    split deploy args into seperate file
    this allows us to include the deploy args in multiple scripts (e.g.
    both overcloud-deploy.sh and overcloud-update-prepare.sh).

---
+++ overcloud-deploy-args.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+# When passing environment files (`-e ...`) to the `overcloud deploy`
+# command, order is important! Your custom configuration
+# (`$PWD/templates/deploy.yaml` in this script) should come *last*,
+# in particular because the network interface configuration is
+# initialized by `deployed-server-environment.yaml`.  If your custom
+# configuration were provided first, the network interface
+# configuration you provide would be lost and replaced with the
+# defaults.
+
+deploy_args=(
+	# network_data.yaml defines a custom network configuration. In 
+	# particular, we disable the StorageMgmt network, since we're not
+	# using it.
+	-n $PWD/templates/network/network_data.yaml
+
+	# Enable network isolation (using different networks for different
+	# purposes).
+	-e $TEMPLATES/environments/network-isolation.yaml
+
+	# Enable deployment onto pre-provisioned servers.
+	-e $TEMPLATES/environments/deployed-server-environment.yaml
+	-e $TEMPLATES/environments/deployed-server-bootstrap-environment-rhel.yaml
+	-e $TEMPLATES/environments/deployed-server-pacemaker-environment.yaml
+	-r $PWD/roles_data.yaml
+
+	# Enable TLS for public endpoints.
+	-e $TEMPLATES/environments/ssl/enable-tls.yaml
+	-e $TEMPLATES/environments/tls-endpoints-public-dns.yaml
+
+	# Enable Neutron Octavia load-balancer service.
+	-e $TEMPLATES/environments/services/octavia.yaml
+
+	# Enable barbican
+	-e $TEMPLATES/environments/services/barbican.yaml
+	-e $TEMPLATES/environments/barbican-backend-simple-crypto.yaml
+
+	# Enable Sahara
+	-e $TEMPLATES/environments/services/sahara.yaml
+
+	# Enable EC2 API
+	-e $TEMPLATES/environments/services/ec2-api.yaml
+
+	# Enable OpenIDC federation
+	#-e $TEMPLATES/environments/enable-federation-openidc.yaml
+
+	# Use Docker registry on the undercloud.
+	-e $PWD/templates/overcloud_images.yaml
+
+	# Enable keystone federation
+	-e $PWD/templates/single-signon.yaml
+
+	# Enable external Ceph cluster
+	-e $TEMPLATES/environments/ceph-ansible/ceph-ansible-external.yaml
+	-e $PWD/templates/ceph-external.yaml
+
+	# Enable external Ceph RadosGW (object storage)
+	-e $TEMPLATES/environments/swift-external.yaml
+	-e $PWD/templates/swift-external.yaml
+
+	# Disable snmp (to be configured outside of Director)
+	-e $PWD/templates/disable-snmp.yaml
+
+	# Most of our custom configuration.
+	-e $PWD/templates/deploy.yaml
+	-e $PWD/templates/controller_extraconfig.yaml
+	-e $PWD/templates/services.yaml
+	-e $PWD/templates/rolecount.yaml
+	-e $PWD/templates/credentials.yaml
+	-e $PWD/templates/fencing.yaml
+
+	# Static ip assignment
+	-e $PWD/templates/hostnamemap.yaml
+	-e $PWD/templates/deployedserverportmap.yaml
+
+	# per https://access.redhat.com/support/cases/#/case/02552401
+	-e $PWD/templates/use_fixed_ips.yaml
+)
+
+if [ -f local_deploy_config.sh ]; then
+	. local_deploy_config.sh
+fi
--- overcloud-update-prepare.sh
+++ overcloud-update-prepare.sh
@@ -13,102 +13,16 @@ else
 	TEMPLATES=/usr/share/openstack-tripleo-heat-templates
 fi
 
-# Generate files
-ansible-playbook playbooks-deploy/playbook.yml
+# Ensure that templates are up-to-date
+(cd playbooks; ansible-playbook deploy.yml)
 
-# When passing environment files (`-e ...`) to the `overcloud deploy`
-# command, order is important! Your custom configuration
-# (`$PWD/templates/deploy.yaml` in this script) should come *last*,
-# in particular because the network interface configuration is
-# initialized by `deployed-server-environment.yaml`.  If your custom
-# configuration were provided first, the network interface
-# configuration you provide would be lost and replaced with the
-# defaults.
+# This is necessary to work around bugs #1645503 and #1645134
+. ./overcloud-env.sh
+bash $TEMPLATES/deployed-server/scripts/enable-ssh-admin.sh
 
-deploy_args=(
-	# network_data.yaml defines a custom network configuration. In 
-	# particular, we disable the StorageMgmt network, since we're not
-	# using it.
-	-n $PWD/templates/network/network_data.yaml
+. overcloud-deploy-args.sh
 
-	# Enable network isolation (using different networks for different
-	# purposes).
-	-e $TEMPLATES/environments/network-isolation.yaml
-
-	# Enable deployment onto pre-provisioned servers.
-	-e $TEMPLATES/environments/deployed-server-environment.yaml
-	-e $TEMPLATES/environments/deployed-server-bootstrap-environment-rhel.yaml
-	-e $TEMPLATES/environments/deployed-server-pacemaker-environment.yaml
-	-r $PWD/roles_data.yaml
-
-	# Enable TLS for public endpoints.
-	-e $TEMPLATES/environments/ssl/enable-tls.yaml
-	-e $TEMPLATES/environments/tls-endpoints-public-dns.yaml
-
-	# Enable Neutron Octavia load-balancer service.
-	-e $TEMPLATES/environments/services/octavia.yaml
-
-	# Enable barbican
-	-e $TEMPLATES/environments/services/barbican.yaml
-	-e $TEMPLATES/environments/barbican-backend-simple-crypto.yaml
-
-	# Enable Sahara
-	-e $TEMPLATES/environments/services/sahara.yaml
-
-	# Enable EC2 API
-	-e $TEMPLATES/environments/services/ec2-api.yaml
-
-	# Enable OpenIDC federation
-	-e $TEMPLATES/environments/enable-federation-openidc.yaml
-
-	# Use Docker registry on the undercloud.
-	-e $PWD/templates/overcloud_images.yaml
-
-	# Enable keystone federation
-	-e $PWD/templates/single-signon.yaml
-
-	# Enable external Ceph cluster
-	-e $TEMPLATES/environments/ceph-ansible/ceph-ansible-external.yaml
-	-e $PWD/templates/ceph-external.yaml
-
-	# Enable external Ceph RadosGW (object storage)
-	-e $TEMPLATES/environments/swift-external.yaml
-	-e $PWD/templates/swift-external.yaml
-
-	# Disable snmp (to be configured outside of Director)
-	-e $PWD/templates/disable-snmp.yaml
-
-	# Most of our custom configuration.
-	-e $PWD/templates/deploy.yaml
-	-e $PWD/templates/rolecount.yaml
-	-e $PWD/templates/credentials.yaml
-	-e $PWD/templates/fencing.yaml
-
-	# Static ip assignment
-	-e $PWD/templates/hostnamemap.yaml
-	-e $PWD/templates/deployedserverportmap.yaml
-)
-
-if [ -d patches/puppet-modules ]; then
-	echo "archiving puppet modules..."
-	tar -cz -f puppet-modules.tar.gz -C patches/puppet-modules .
-
-	echo "uploading puppet modules..."
-	upload-swift-artifacts \
-		-f puppet-modules.tar.gz \
-		--environment $PWD/templates/puppet_modules.yaml
-
-	sed -i s/DeployArtifactURLs/PuppetModuleUrls/ \
-		$PWD/templates/puppet_modules.yaml
-
-	deploy_args+=(-e $PWD/templates/puppet_modules.yaml)
-fi
-
-if [ -f local_deploy_config.sh ]; then
-	. local_deploy_config.sh
-fi
-
-openstack overcloud update prepare \
+openstack overcloud udpate prepare \
 	--templates $TEMPLATES \
 	--disable-validations --deployed-server \
 	--libvirt-type kvm \
